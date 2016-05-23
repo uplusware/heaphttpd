207f8d9a226fdded335b893bdddf7890cab1e103
Uplusware <uplusware@gmail.com>
2016-05-23 10:03:09 +0800

    0.3
    0.3

--- webpy/main.py
+++ webpy/main.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 
 import web
-
+import string
 urls = ("/webpy/.*", "hello")
 app = web.application(urls, globals())
 
 class hello: 
     def GET(self):
-        return 'Hello, world!'
+        return 'Hello, GET!'
+    def POST(self):
+        i = web.input();
+        return 'Hello, POST! ' + i.abcd + " + " + i.efgh + " = " + str((string.atoi(i.abcd) + string.atoi(i.efgh)))
 
 web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
 if __name__ == "__main__":
