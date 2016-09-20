1ec07e671960b2ed3657244611f34263e87a1dbc
Uplusware <uplusware@gmail.com>
2016-09-20 12:59:30 +0800

    0.3
    0.3

--- src/mainsrv.cpp
+++ src/mainsrv.cpp
@@ -47,8 +47,8 @@ static void daemon_init()
 	chdir("/");
 	umask(0);
 	close(STDIN_FILENO);
-	//close(STDOUT_FILENO);
-	//close(STDERR_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
 }
 
 char PIDFILE[256] = "/tmp/niuhttpd/niuhttpd.pid";
