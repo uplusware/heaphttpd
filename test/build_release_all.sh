f0c8f384d0227e00230ffdc7ed9cb906e5217f5b
Uplusware <uplusware@gmail.com>
2018-12-07 13:55:12 +0800

    1.1
    1.1

--- test/build_release_all.sh
+++ test/build_release_all.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
 cd ${SCRIPT_DIR}/src/
+
 make clean
 make
 cd ${SCRIPT_DIR}
 chmod a+x ${SCRIPT_DIR}/release.sh
-${SCRIPT_DIR}/release.sh 1.1 threads centos7
+${SCRIPT_DIR}/release.sh 1.1 threads ubuntu16lts
+
 cd ${SCRIPT_DIR}/src/
 make clean
 make ASYNC=1
 cd ${SCRIPT_DIR}
 chmod a+x ${SCRIPT_DIR}/release.sh
-${SCRIPT_DIR}/release.sh 1.1 async centos7
-
+${SCRIPT_DIR}/release.sh 1.1 async ubuntu16lts
